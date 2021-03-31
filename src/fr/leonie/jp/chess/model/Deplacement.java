ea8b5a99d75e6b07955ce1a6eb2cac1d7fc0bb3a
J-Phiz <jp.save@gmail.com>
2021-03-31 18:30:47 +0200

    Gestion du deplacement d'une piece sur une case vide ou à l'adversaire

--- src/fr/leonie/jp/chess/model/Deplacement.java
+++ src/fr/leonie/jp/chess/model/Deplacement.java
@@ -6,4 +6,21 @@ public class Deplacement {
     private Carreau careauDepart;
     private Carreau carreauFin;
 
+    public Deplacement(Piece piece, Carreau careauDepart, Carreau carreauFin) {
+        this.piece = piece;
+        this.careauDepart = careauDepart;
+        this.carreauFin = carreauFin;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public Carreau getCareauDepart() {
+        return careauDepart;
+    }
+
+    public Carreau getCarreauFin() {
+        return carreauFin;
+    }
 }
