ab23380406dbc42d83b9d5d1b1fa07399f656012
J-Phiz <jp.save@gmail.com>
2021-04-01 11:17:29 +0200

    Vérifier qu'il y a des déplacements possibles avant de les afficher

--- src/fr/leonie/jp/chess/controller/ChessController.java
+++ src/fr/leonie/jp/chess/controller/ChessController.java
@@ -149,7 +149,9 @@ public class ChessController implements Initializable {
     }
 
     private void showAllowMoves(ArrayList<Deplacement> deplacements) {
-        deplacements.forEach(deplacement -> deplacement.getCarreauFin().setDestination(true));
+        if (deplacements != null) {
+            deplacements.forEach(deplacement -> deplacement.getCarreauFin().setDestination(true));
+        }
     }
 
     private void cancelButtonInit() {
