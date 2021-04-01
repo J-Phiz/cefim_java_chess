172a30438134f5f05a97f3b8c246e45d814f685e
Léonie Dusart <leonie.dusart@gmail.comm>
2021-04-01 11:45:01 +0200

    queen moves

--- src/fr/leonie/jp/chess/model/Reine.java
+++ src/fr/leonie/jp/chess/model/Reine.java
@@ -16,7 +16,163 @@ public class Reine extends Piece {
 
     @Override
     public ArrayList<Deplacement> deplacementsPossibles(Plateau plateau, Carreau carreau) {
-        return null;
+        ArrayList<Deplacement> deplacements = new ArrayList<>();
+        int indexColonneDepart = carreau.getColonne();
+        int indexLigneDepart = carreau.getLigne();
+
+        // déplacement vers la droite
+        for(int i = 1; i < 8; i++) {
+            if (indexColonneDepart + i >= 0 && indexColonneDepart + i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart + i][indexLigneDepart];
+                if (destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement vers la gauche
+        for(int i = 1; i < 8; i++) {
+            if (indexColonneDepart - i >= 0 && indexColonneDepart - i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart - i][indexLigneDepart];
+                if (destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement vers le haut
+        for(int i = 1; i < 8; i++) {
+            if (indexLigneDepart + i >= 0 && indexLigneDepart + i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart][indexLigneDepart + i];
+                if (destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement vers le bas
+        for(int i = 1; i < 8; i++) {
+            if (indexLigneDepart - i >= 0 && indexLigneDepart - i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart][indexLigneDepart - i];
+                if (destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement diagonale haut/droit
+        for(int i = 1; i < 8; i++) {
+            if (indexColonneDepart + i >= 0 && indexColonneDepart + i < 8 && indexLigneDepart + i >= 0 && indexLigneDepart + i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart + i][indexLigneDepart + i];
+                if (destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement diagonale bas/droit
+        for(int i = 1; i < 8; i++) {
+            if(indexColonneDepart + i >= 0 && indexColonneDepart + i < 8 && indexLigneDepart - i >= 0 && indexLigneDepart - i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart + i][indexLigneDepart - i];
+                if(destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement diagonale haut/gauche
+        for(int i = 1; i < 8; i++) {
+            if (indexColonneDepart - i >= 0 && indexColonneDepart - i < 8 && indexLigneDepart + i >= 0 && indexLigneDepart + i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart - i][indexLigneDepart + i];
+                if (destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        // déplacement diagonale bas/gauche
+        for(int i = 1; i < 8; i++) {
+            if(indexColonneDepart - i >= 0 && indexColonneDepart - i < 8 && indexLigneDepart - i >= 0 && indexLigneDepart - i < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart - i][indexLigneDepart - i];
+                if(destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+                if(destination.getContenu() != null) {
+                    break;
+                }
+            }
+        }
+
+        return deplacements;
     }
 
 }
