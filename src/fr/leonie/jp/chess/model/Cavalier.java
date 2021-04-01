945f8f48841ec8252c51494e39b2cd1e6fa848ea
Léonie Dusart <leonie.dusart@gmail.comm>
2021-04-01 11:54:27 +0200

    knight moves

--- src/fr/leonie/jp/chess/model/Cavalier.java
+++ src/fr/leonie/jp/chess/model/Cavalier.java
@@ -16,7 +16,29 @@ public class Cavalier extends Piece {
 
     @Override
     public ArrayList<Deplacement> deplacementsPossibles(Plateau plateau, Carreau carreau) {
-        return null;
+        ArrayList<Deplacement> deplacements = new ArrayList<>();
+        int indexColonneDepart = carreau.getColonne();
+        int indexLigneDepart = carreau.getLigne();
+
+        int[][] targets = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, -1}};
+
+        for(int i = 0; i < targets.length; i++) {
+            if(indexColonneDepart + targets[i][0] >= 0 && indexColonneDepart + targets[i][0] < 8 && indexLigneDepart + targets[i][1] >= 0 && indexLigneDepart + targets[i][1] < 8) {
+                Carreau destination = plateau.getCarreaux()[indexColonneDepart + targets[i][0]][indexLigneDepart + targets[i][1]];
+
+                if(destination.getContenu() == null || destination.getContenu().getCouleur() != this.getCouleur()) {
+                    Deplacement deplacement = new Deplacement(
+                            this,
+                            carreau,
+                            destination,
+                            destination.getContenu()
+                    );
+                    deplacements.add(deplacement);
+                }
+            }
+        }
+
+        return deplacements;
     }
 
 }
